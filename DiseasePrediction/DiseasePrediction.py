d5f6ed3774cdc662cf604ee0885d80daa8094ec4
c0r2a-lab <richardjkennedy9699@gmail.com>
2024-01-29 15:21:48 +0800

    update: understood all of logic

--- DiseasePrediction/DiseasePrediction.py
+++ DiseasePrediction/DiseasePrediction.py
@@ -83,7 +83,7 @@ nb_model = GaussianNB()
 nb_model.fit(X_train, y_train)
 preds = nb_model.predict(X_test)
 print(f"Accuracy on train data by Naive Bayes Classifier\
-      : {accuracy_score(y_train, nb_model.predict(X_train)) * 100")
+      : {accuracy_score(y_train, nb_model.predict(X_train)) * 100}")
 
 print(f"Accuracy on test data by Naive Bayes Classifier\
 : {accuracy_score(y_test, preds)*100}      ")
@@ -93,3 +93,95 @@ sns.heatmap(cf_matrix, annot=True)
 plt.title("Confusion Matrix for Naive Bayes Classifier on Test Data")
 plt.show()
 
+# Training and testing Random Forest Classifier
+rf_model = RandomForestClassifier(random_state=18)
+rf_model.fit(X_train, y_train)
+preds = rf_model.predict(X_test)
+print(f"Accuracy on train data by Rnadom Forest Classifier\
+: {accuracy_score(y_train, rf_model.predict(X_train))*100}      ")
+
+print(f"Accuracy on test data by Random Forest Classifier\
+: {accuracy_score(y_test, preds)*100}      ")
+
+cf_matrix = confusion_matrix(y_test, preds)
+plt.figure(figsize=(12,8))
+sns.heatmap(cf_matrix, annot=True)
+plt.title("Confusion Matrix for Random Forest Classifer on Test Data")
+plt.show()
+
+# Training the models on whole data
+final_svm_model = SVC()
+final_nb_model = GaussianNB()
+final_rf_model = RandomForestClassifier(random_state=18)
+final_svm_model.fit(X,y)
+final_nb_model.fit(X,y)
+final_rf_model.fit(X,y)
+
+# Reading the test data
+test_data = pd.read_csv("./dataset/Testing.csv").dropna(axis=1)
+
+test_X = test_data.iloc[:, :-1]
+test_Y = encoder.transform(test_data.iloc[:,-1])
+
+# Making prediction by take mode of predictions
+# made by all the classifiers
+svm_preds = final_svm_model.predict(test_X)
+nb_preds = final_nb_model.predict(test_X)
+rf_preds = final_rf_model.predict(test_X)
+
+final_preds = [mode([i,j,k])[0][0] for i,j,k in zip(svm_preds, nb_preds, rf_preds)]
+
+print(f"Accuracy on Test dataset by the combined model\
+: {accuracy_score(test_Y, final_preds)*100}      ")
+
+cf_matrix = confusion_matrix(test_Y, final_preds)
+plt.figure(figsize=(12,8))
+
+sns.heatmap(cf_matrix, annot=True)
+plt.title("Confusion Matrix for Combined Model on Test Dataset")
+plt.show()
+
+symptoms = X.columns.values
+
+# Creating a symptom index dictionary to encode the
+# input symptoms into numerical form
+symptom_index = {}
+for index, value in enumerate(symptoms):
+    symptom = " ".join([i.capitalize() for i in value.split("_")])
+    symptom_index[symptom] = index
+    
+data_dict = {
+    "symptom_index": symptom_index,
+    "predictions_classes": encoder.classes_}
+
+# Defining the Function
+# input: string containing symptoms separated by commas
+# output: generated predictions by models
+def predictDisease(symptoms):
+    symptoms = symptoms.split(",")
+    # creating input data for the models
+    input_data = [0] * len(data_dict["symptom_index"])
+    for symptom in symptoms:
+        index = data_dict["symptom_index"][symptom]
+        input_data[index] = 1
+        
+    # reshaping the input data and converting it
+    # into suitable format for model predictions
+    input_data = np.array(input_data).reshape(1, -1)
+    
+    # generating individual outputs
+    rf_prediction = data_dict["predictions_classes"][final_rf_model.predict(input_data)[0]]
+    nb_prediction = data_dict["predictions_classes"][final_nb_model.predict(input_data)[0]]
+    svm_prediction = data_dict["predictions_classes"][final_svm_model.predict(input_data)[0]]
+    
+    # making final prediction by taking mode of all predictions
+    final_prediction = mode([rf_prediction, nb_prediction, svm_prediction])[0][0]
+    predictions = {
+        "rf_model_prediction": rf_prediction,
+        "naive_bayes_prediction": nb_prediction,
+        "svm_model_prediction": svm_prediction,
+        "final_prediction": final_prediction}
+    return predictions
+
+# Testing the function
+print(predictDisease("Itching,Skin Rash,Nodal Skin Eruptions"))
